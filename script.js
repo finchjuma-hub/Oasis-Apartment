34be181613a8023cb5f501cf9d1691b0a8bdaf49
Finch Juma <finch.juma@student.moringaschool.com>
2026-02-16 21:58:28 +0300

    Update booking section

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function (){
     const userData = JSON.parse(localStorage.getItem("userData"));
 
     if (!userData){
-        alert("No search data found. Please search for a room first.");
+        // alert("No search data found. Please search for a room first.");
         window.location.href = "index.html";
         return;
     }
