184e3de18e251406f21a081cb5038abf4f5f8a8f
Finch Juma <finch.juma@student.moringaschool.com>
2026-02-16 07:16:53 +0300

    Initialize all rooms

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", function (){
         window.location.href = "booking.html";
         return;
     }
+
+    // Initialize rooms
+    if (!localStorage.getItem("rooms")){
+        const rooms = [
+            {id: 1, name: "Two Bedroom Apartment", bookings:[], },
+            {id: 2, name: "One Bedroom Apartment", bookings:[], },
+            {id: 3, name: "Studio Apartment", bookings:[], },
+        ];
+        localStorage.setItem("rooms", JSON.stringify(rooms));
+    }
+    let rooms = JSON.parse(localStorage.getItem("rooms"));
+
+
+
+    
