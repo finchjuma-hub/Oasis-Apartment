4549d30aaa93a542e58b0f256a11b8ecc1354eca
Finch Juma <finch.juma@student.moringaschool.com>
2026-02-16 07:35:31 +0300

    Display rooms

--- script.js
+++ script.js
@@ -43,3 +43,35 @@ document.addEventListener("DOMContentLoaded", function (){
         });
     }
 
+    // Display rooms
+    function displayRooms(){
+        const container = document.getElementById("room-container");
+        container.innerHTML = "";
+
+        let availableCount = 0;
+
+        rooms.forEach(room => {
+            const available = isRoomAvailable(room);
+            if(available) availableCount++;
+
+            const div = document.createElement("div");
+            div.className = "room-card";
+
+            div.innerHTML = `
+                <h3>${room.name}</h3>
+                <p>Status:
+                     <strong class="${available ? 'available' : 'booked'}">
+                     ${available ? "Available" : "Booked"}
+                     </strong>
+                </p>
+                ${available
+                    ? `<button onclick="bookRoom(${room.id})">Book Now</button>`
+                    : `<button disabled>Unavailable</button>`}
+            `;
+
+            container.appendChild(div);
+        });
+        if (availableCount === 0){
+            container.innerHTML = "<p>NO rooms available for selected dates.</p>";
+        }
+    }
